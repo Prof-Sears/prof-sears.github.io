d99fc37da93a2bd5ff29290ea6224654a2245e2e
Chris Sears <omegalimit.sears@gmail.com>
2020-11-26 13:16:57 -0500

    FIxed CSV file

--- Learning/CSVData/KYcovid.js
+++ Learning/CSVData/KYcovid.js
@@ -2,7 +2,13 @@ console.log('Fetching Data');
 
 async function loadData() {
     const response = await fetch('https://prof-sears.github.io/Learning/CSVData/KY_COVID_data.csv'); // Load data from CSV file
-    const dataset = await response.text();             // Get the response as text;
-    console.log(dataset);
+    const rawtext = await response.text();                                                           // Get the response as text;
+    
+    rows = rawtext.split('\n');                  // Split the CSV file into rows.
+    headers = rows[0].split(',');                // Split the first row as a header.
+    console.log(`Read CSV headers: ${headers}`); // Record the headers to the console.
+    for(let i = 1; i < rows.length; i++) {
+        console.log(rows[i]);
+    }
 }
 
