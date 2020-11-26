3f1f7f67072af4f8a1948480eaba8789ad802873
Chris Sears <omegalimit.sears@gmail.com>
2020-11-26 13:00:01 -0500

    Still Building

--- Learning/CSVData/KYcovid.js
+++ Learning/CSVData/KYcovid.js
@@ -1,1 +1,8 @@
+console.log('Fetching Data');
+
+async function loadData() {
+    const response = await fetch('https://prof-sears.github.io/Learning/CSVData/KY_COVID_data.csv'); // Load data from CSV file
+    const dataset = await response.text();             // Get the response as text;
+    console.log(dataset);
+}
 
