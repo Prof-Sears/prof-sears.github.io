528e2c602992a91a6c6ff16771b5b9877ccef2ae
Chris Sears <omegalimit.sears@gmail.com>
2020-11-26 12:52:44 -0500

    Fix Spelling

--- Learning/CVSData/KYcovid.js
+++ Learning/CVSData/KYcovid.js
@@ -1,7 +1,1 @@
-console.log('Fetching Data');
 
-async function loadData() {
-    const response = await fetch('file:///KY_COVID_data.csv'); // Load data from CSV file
-    const dataset = await response.text();             // Get the response as text;
-    console.log(dataset);
-}
