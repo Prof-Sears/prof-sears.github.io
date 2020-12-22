4b29362b768c36b4502106331b6b752200da5043
Chris Sears <omegalimit.sears@gmail.com>
2020-12-22 13:11:20 -0500

    Advent of Code - Day 17 fizzle

---
+++ AdventOC/2020-12-17/ConwayCubes.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+console.log('Conway Cubes running...');
+
+const solvePart1 = () => {
+    console.log('Solving Part 1.');
+
+    console.log(`Part 1 Solution.`);
+}
+
+fs.readFile('input.txt', solvePart1);
