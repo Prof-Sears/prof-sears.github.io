25b8699c16ac4b86519a3b850d440f8065ceb1ee
Chris Sears <omegalimit.sears@gmail.com>
2021-02-25 08:38:40 -0500

    Update ConwayCubes.js
    adsfa

--- AdventOC/2020-12-17/ConwayCubes.js
+++ AdventOC/2020-12-17/ConwayCubes.js
@@ -2,7 +2,12 @@ const fs = require('fs');
 
 console.log('Conway Cubes running...');
 
+const loadLines => () {
+    
+}
+
 const solvePart1 = () => {
+    loadLines();
     console.log('Solving Part 1.');
 
     console.log(`Part 1 Solution.`);
